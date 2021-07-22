cdbe585730d9e4ef6674c72ede2890659400aa0e
tangze <mf1832146@smail.nju.edu.cn>
2021-07-22 14:31:05 +0800

    [init]

--- module/components.py
+++ module/components.py
@@ -281,16 +281,15 @@ class DecoderLayer(nn.Module):
         super(DecoderLayer, self).__init__()
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
+        self.feed_forward = FeedForward(d_model, dim_feedforward, dropout=dropout)
+        self.sublayer = _get_clones(SublayerConnection(d_model, dropout), 3)
 
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.norm3 = nn.LayerNorm(d_model)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.dropout3 = nn.Dropout(dropout)
+        # self.norm1 = nn.LayerNorm(d_model)
+        # self.norm2 = nn.LayerNorm(d_model)
+        # self.norm3 = nn.LayerNorm(d_model)
+        # self.dropout1 = nn.Dropout(dropout)
+        # self.dropout2 = nn.Dropout(dropout)
+        # self.dropout3 = nn.Dropout(dropout)
 
         self.activation = _get_activation_fn(activation)
 
@@ -301,17 +300,25 @@ class DecoderLayer(nn.Module):
 
     def forward(self, tgt, memory, tgt_mask=None, memory_mask=None,
                 tgt_key_padding_mask=None, memory_key_padding_mask=None):
-        tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
-        tgt = tgt + self.dropout1(tgt2)
-        tgt = self.norm1(tgt)
-        tgt2, attn_weights = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask,
-                                                 key_padding_mask=memory_key_padding_mask)
-        tgt = tgt + self.dropout2(tgt2)
-        tgt = self.norm2(tgt)
-        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
-        tgt = tgt + self.dropout3(tgt2)
-        tgt = self.norm3(tgt)
+        # tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask,
+        #                       key_padding_mask=tgt_key_padding_mask)[0]
+        # tgt = tgt + self.dropout1(tgt2)
+        # tgt = self.norm1(tgt)
+        # tgt2, attn_weights = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask,
+        #                                          key_padding_mask=memory_key_padding_mask)
+        # tgt = tgt + self.dropout2(tgt2)
+        # tgt = self.norm2(tgt)
+        # tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        # tgt = tgt + self.dropout3(tgt2)
+        # tgt = self.norm3(tgt)
+        tgt, attn_weights = self.sublayer[0](tgt, lambda x: self.self_attn(x, x, x, attn_mask=tgt_mask,
+                                                                           key_padding_mask=tgt_key_padding_mask))
+
+        tgt, attn_weights = self.sublayer[1](tgt, lambda x: self.multihead_attn(
+            x, memory, memory, attn_mask=memory_mask, key_padding_mask=memory_key_padding_mask
+        ))
+
+        tgt, _ = self.sublayer[2](tgt, self.feed_forward)
         return tgt, attn_weights
 
 
